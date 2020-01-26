62c936cdea5a03888bcba871b67633f2305d8b3f
Tomasz Mittlener <t.mittlener@gmail.com>
2020-01-26 21:24:22 +0100

    feat(client): add search results container and create grid

--- packages/client/src/pages/Home/index.tsx
+++ packages/client/src/pages/Home/index.tsx
@@ -1,17 +1,20 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
 import Header from 'containers/Header'
+import SearchResults from 'containers/SearchResults'
 
 const PageWrapper = styled.main`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `
 
 const HomePage: FunctionComponent = () => {
   return (
     <PageWrapper>
       <Header />
+      <SearchResults />
     </PageWrapper>
   )
 }
