bce3bd7a646cde4388d166f6bf8e0f18e705e59b
Tomasz Mittlener <t.mittlener@gmail.com>
2020-01-27 19:07:07 +0100

    feat(server): add pixabay and giphy services

---
+++ packages/server/src/services/imagesService/services/giphyService/index.ts
@@ -0,0 +1,29 @@
+import Axios, { AxiosPromise } from 'axios'
+import { stringify } from 'querystring'
+import { SearchQuery, SearchQueryKeys } from '@gifduck/common-types/imagesService'
+import { GiphyImageResponse, GiphySearchQuery } from './types'
+
+interface GiphyService {
+  search: (searchQuery: SearchQuery) => AxiosPromise<GiphyImageResponse>
+}
+
+const GIPHY_API_KEY = process.env.GIPHY_API_KEY || ''
+const BASE_URL = 'https://api.giphy.com/v1'
+
+const instance = Axios.create({
+  baseURL: BASE_URL,
+})
+
+const getGiphyQuery = (apiKey: string, searchQuery: SearchQuery): GiphySearchQuery => {
+  return {
+    ['api_key']: apiKey,
+    q: searchQuery[SearchQueryKeys.searchText],
+  }
+}
+
+const giphyService: GiphyService = {
+  search: searchQuery => {
+    const query = getGiphyQuery(GIPHY_API_KEY, searchQuery)
+    return instance.get(`/gifs/search?${stringify(query)}`)
+  },
+}
---
+++ packages/server/src/services/imagesService/services/pixabayService/index.ts
@@ -0,0 +1,29 @@
+import Axios, { AxiosPromise } from 'axios'
+import { stringify } from 'querystring'
+import { SearchQuery, SearchQueryKeys } from '@gifduck/common-types/imagesService'
+import { PixabayImageResponse, PixabaySearchQuery } from './types'
+
+interface PixabayService {
+  search: (searchQuery: SearchQuery) => AxiosPromise<PixabayImageResponse>
+}
+
+const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY || ''
+const BASE_URL = 'https://pixabay.com/api'
+
+const instance = Axios.create({
+  baseURL: BASE_URL,
+})
+
+const getPixabayQuery = (apiKey: string, searchQuery: SearchQuery): PixabaySearchQuery => {
+  return {
+    key: apiKey,
+    q: searchQuery[SearchQueryKeys.searchText],
+  }
+}
+
+const pixabayService: PixabayService = {
+  search: searchQuery => {
+    const query = getPixabayQuery(PIXABAY_API_KEY, searchQuery)
+    return instance.get(`/gifs/search?${stringify(query)}`)
+  },
+}
